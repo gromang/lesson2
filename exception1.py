e17ef5e6887fac5dae3fb3d1d8f61241b97f946f
Moiseev Roman <lp15.mr@yandex.ru>
2019-11-26 20:26:47 +0300

    Выполнено

--- exception1.py
+++ exception1.py
@@ -10,11 +10,28 @@
     
 """
 
+
 def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    def ask_user_dict(ask):
+        dictionary = {"Как дела?": "Хорошо!", "Что делаешь?": "Программирую",
+                      "Сколько тебе лет?": "34", "Как тебя зовут?": "Роман"}
+        ask = ask.lstrip()
+        answer = dictionary.get(ask, "Я не знаю что ответить")
+        return answer
+
+    print('Задайте Ваш вопрос\n')
+
+    while True:
+        try:
+            ask = input('Пользователь:')
+            print(f'Программа: {ask_user_dict(ask)}')
+        except KeyboardInterrupt:
+            print('Пока!')
+            break
+
+
 if __name__ == "__main__":
     ask_user()
